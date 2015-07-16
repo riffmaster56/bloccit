2b3e863c3bff225fd289a9191d6c82f04fe5bee5
Alex <riffmaster56@gmail.com>
2015-07-16 13:47:39 -0700

    Added post and comment with unique title and body

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,6 +18,16 @@
      body: Faker::Lorem.paragraph
    )
  end
+
+unique_post = {title: 'My unique Post Title', body: 'My super unique post.'}
+unless Post.where(unique_post).count > 0
+  p = Post.create(unique_post)
+  p.comments.create(body: 'My comment on my super unique post.')
+  puts "Unique Post with unique comment Created"
+end
+
+
+
  
  puts "Seed finished"
  puts "#{posts.count} posts created"
