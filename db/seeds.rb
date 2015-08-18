3e96f4371b95a9a0ca3168b66e03d7df663641b9
Alex <riffmaster56@gmail.com>
2015-08-18 16:54:18 -0700

    Added users and post data to post controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,53 @@
  require 'faker'
+
+ # Create Users
+ 5.times do
+   user = User.new(
+     name:     Faker::Name.name,
+     email:    Faker::Internet.email,
+     password: Faker::Lorem.characters(10)
+   )
+   user.skip_confirmation!
+   user.save!
+ end
+ users = User.all
  
+ # Note: by calling `User.new` instead of `create`,
+ # we create an instance of User which isn't immediately saved to the database.
+ 
+ # The `skip_confirmation!` method sets the `confirmed_at` attribute
+ # to avoid triggering an confirmation email when the User is saved.
+ 
+ # The `save` method then saves this User to the database.
+
  # Create Posts
- posts = []
  50.times do
-   posts << Post.create!(
+   Post.create!(
+     user:   users.sample,
      title:  Faker::Lorem.sentence,
      body:   Faker::Lorem.paragraph
    )
  end
- # posts = Post.all
- 
+ posts = Post.all
+
  # Create Comments
- comments = []
  100.times do
-   comments << Comment.create!(
+   Comment.create!(
+     # user: users.sample,   # we have not yet associated Users with Comments
      post: posts.sample,
      body: Faker::Lorem.paragraph
    )
  end
+
+ user = User.first
+ user.skip_reconfirmation!
+ user.update_attributes!(
+  email: 'riffmaster56@gmail.com',
+  password: 'guitars24u'
+  )
  
  puts "Seed finished"
- puts "#{posts.count} posts created"
- puts "#{comments.count} comments created"
+ puts "#{User.count} users created"
+ puts "#{Post.count} posts created"
+ puts "#{Comment.count} comments created"
+
