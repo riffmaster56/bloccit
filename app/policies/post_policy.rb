9dee34c60161e5a6c0bb8ccfa101f24e7767e325
Alex <riffmaster56@gmail.com>
2015-08-25 14:12:06 -0700

    Changed authorization for moderator member and guest

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,5 +1,26 @@
 class PostPolicy < ApplicationPolicy
 
+  class Scope
+    attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      if user.present?
+        if user.admin? || user.moderator?
+          scope.all
+        else
+          scope.where(:user_id => user.id)
+        end
+      else
+        scope.none
+      end
+    end
+  end
+
   def index?
     true
   end
