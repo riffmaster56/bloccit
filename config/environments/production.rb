0e127710245fbf329e182d52e42e915c32e5a9ee
Alex <riffmaster56@gmail.com>
2015-08-27 16:13:03 -0700

    Configured app for Production emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,9 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  # http://bloccit.herokuapp.com
+   config.action_mailer.default_url_options = { host: 'bloccit.herokuapp.com' }
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
@@ -42,7 +45,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
