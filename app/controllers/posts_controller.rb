32bb6b416fe1085bf3042c49e0172fe1a52b3690
Alex <riffmaster56@gmail.com>
2015-08-31 04:38:07 -0700

    Added user_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,7 +14,7 @@ class PostsController < ApplicationController
 
   def create
     @topic = Topic.find(params[:topic_id])
-    @post = Post.new(params.require(:post).permit(:title, :body))
+    @post = Post.new(post_params)
     @post.user = current_user
     @post.topic =@topic
     authorize @post
@@ -37,7 +37,7 @@ class PostsController < ApplicationController
       @topic = Topic.find(params[:topic_id])
      @post = Post.find(params[:id])
      authorize @post
-     if @post.update_attributes(params.require(:post).permit(:title, :body))
+     if @post.update_attributes(post_params)
        flash[:notice] = "Post was updated."
        redirect_to [@topic, @post]
      else
@@ -45,4 +45,10 @@ class PostsController < ApplicationController
        render :edit
      end
    end
+end
+
+private
+
+def post_params
+  params.require(:post).permit(:title, :body)
 end
