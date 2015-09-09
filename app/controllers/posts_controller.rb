d3538f62a21316a99adc347597b6abfed192cac7
Alex <riffmaster56@gmail.com>
2015-09-09 10:04:29 -0700

    added delete to posts and topics

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -34,7 +34,7 @@ class PostsController < ApplicationController
   end
 
      def update
-      @topic = Topic.find(params[:topic_id])
+     @topic = Topic.find(params[:topic_id])
      @post = Post.find(params[:id])
      authorize @post
      if @post.update_attributes(post_params)
@@ -43,6 +43,19 @@ class PostsController < ApplicationController
      else
        flash[:error] = "There was an error saving the post. Please try again."
        render :edit
+     end
+   end
+     def destroy
+     @topic = Topic.find(params[:topic_id])
+     @post = Post.find(params[:id])
+     authorize @post
+ 
+     if @post.destroy
+       flash[:notice] = "\"#{@post.title}\" was deleted successfully."
+       redirect_to @topic
+     else
+       flash[:error] = "There was an error deleting the post."
+       render :show
      end
    end
 end
