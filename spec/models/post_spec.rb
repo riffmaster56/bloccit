93c5905be92caabf0b7ba11323d848252a7e2a67
Alex <riffmaster56@gmail.com>
2015-09-17 14:33:37 -0700

    added rspec validations

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,4 +1,4 @@
- require 'rails_helper'
+require 'rails_helper'
  
  describe Post do
    describe "vote methods" do
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe Vote, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Vote do
+  describe "validations" do
+    describe "value validation" do
+      it "only allows -1 or 1 as values" do
+        expect(Vote.new(value: 1).valid?).to eq(true)
+        expect(Vote.new(value: -1).valid?).to eq(true)
+        expect(Vote.new(value: 2).valid?).to eq(false)
+      end
+    end
+  end
 end
