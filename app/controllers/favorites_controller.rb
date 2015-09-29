b4b7fc9c6c30d9f7c5c52ebb1b924ca9cba29363
Alex <riffmaster56@gmail.com>
2015-09-29 05:33:28 -0700

    completed favoriting checkpoint

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,12 +3,22 @@ class FavoritesController < ApplicationController
   def create
     post = Post.find(params[:post_id])
     favorite = current_user.favorites.build(post: post)
- 
+  authorize favorite
     if favorite.save
       # Add code to generate a success flash and redirect to post
       # Remember the path shortcut: [post.topic, post]
     else
       # Add code to generate a failure flash and redirect to post
+    end
+  end
+  def destroy
+   # Get the post from the params
+   # Find the current user's favorite with the ID in the params
+  authorize favorite
+    if favorite.destroy
+     # Flash success and redirect to @post
+    else
+     # Flash error and redirect to @post
     end
   end
 end
